07f13df60afe102ce30506088e063233ff592d09
Satyarth Pandey <satyarth2002pandey@gmail.com>
2021-06-17 12:10:32 +0530

    Use Flask Template

--- app.py
+++ app.py
@@ -1,6 +1,49 @@
 from flask import *
-
 from main import *
+
+
+def set_html(tweets):
+
+    # picking positive tweets from tweets
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    # picking negative tweets from tweets
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    # picking neutral tweets from tweets
+    neutweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral']
+    textTweet = []
+    cleanTweet = []
+    sentimentTweet = []
+    for i in range(0, len(tweets)):
+        textTweet.append(tweets[i]['text'])
+    for i in range(0, len(tweets)):
+        cleanTweet.append(tweets[i]['clean tweet'])
+    for i in range(0, len(tweets)):
+        sentimentTweet.append(tweets[i]['sentiment'])
+
+    # Create a dataframe of collected tweets
+    df = pd.concat([
+        pd.DataFrame(textTweet, columns=['tweet']),
+        pd.DataFrame(cleanTweet, columns=['cleantweet'])
+    ],axis=1)
+    df = pd.concat([df, pd.DataFrame(sentimentTweet, columns=['sentiment'])], axis=1)
+
+    wcloud = ' '.join([i for i in df['cleantweet']])
+
+    wordcloud = WordCloud(width=1000,
+                          height=700,
+                          random_state=21,
+                          max_font_size=120).generate(wcloud)
+    dir_path = os.path.dirname(__file__)
+
+    for fname in os.listdir(dir_path + '/static/css/images'):
+      if fname.startswith('img_'):
+        os.remove(dir_path + "/static/css/images/" + fname)
+
+    image_name = "img_{}.png".format(str(uuid.uuid4()))
+    wordcloud.to_file('static/css/images/{}'.format(image_name))
+    
+    return ptweets,ntweets,neutweets,image_name
+
 
 app = Flask(__name__)
 
@@ -18,16 +61,11 @@ def display():
         api = TwitterClient()
         # calling function to get tweets
         tweets = api.get_tweets(userQuery)
-        set_html(tweets)
-        return redirect(f'result/{userQuery}')
+        ptweets, ntweets, neutweets, image_name = set_html(tweets)
+        return render_template('index.html', pos_tweets=ptweets, neg_tweets=ntweets, neutral_tweets=neutweets, img_name=image_name)
 
     else:
         return render_template('inputMain.html')
-
-
-@app.route('/result/<userQuery>')
-def result(userQuery):
-    return render_template("copyindex.html")
 
 
 if (__name__ == "__main__"):
--- main.py
+++ main.py
@@ -6,20 +6,18 @@ from wordcloud import WordCloud
 import pandas as pd
 import re
 import os
-from wordcloud import WordCloud
-from shutil import copyfile
-
+from dotenv import load_dotenv
 
 image_name = ""
-
+load_dotenv()
 
 class TwitterClient(object):
     def __init__(self):
         # Twitter API credentials
-        consumer_key = 'HE9pwjf1g85QO9PlIGedNyg0k'
-        consumer_secret = 'RITiKSR4pZZRGPVFKIq6nEdokh8D33WMTH0O8MckJWvYzP6bjN'
-        access_token = '1353286031467040776-kmjCmVoQHPYaVHCPMixYToWUgLmOM1'
-        access_token_secret = 'KYvwJwGFzjXUWWyokvcWWCBrhpRsqtzaO5oCbCYyUGfA7'
+        consumer_key = os.getenv('consumer_key')
+        consumer_secret = os.getenv('consumer_secret')
+        access_token =  os.getenv('access_token')
+        access_token_secret =  os.getenv('access_token_secret')
 
         # authentication
         try:
@@ -96,175 +94,4 @@ class TwitterClient(object):
             # print error (if any)
             print("Error : " + str(e))
 
-
 # class ends
-
-
-def set_html(tweets):
-
-    # picking positive tweets from tweets
-    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-    # percentage of positive tweets
-    print("Positive tweets percentage: {} %".format(100 * len(ptweets) /
-                                                    len(tweets)))
-    # picking negative tweets from tweets
-    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-    # picking neutral tweets from tweets
-    neutweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral']
-
-    # percentage of negative tweets
-    print("Negative tweets percentage: {} %".format(100 * len(ntweets) /
-                                                    len(tweets)))
-    # percentage of neutral tweets
-    print("Neutral tweets percentage: {} %".format(
-        100 * (len(tweets) - (len(ntweets) + len(ptweets))) / len(tweets)))
-
-    textTweet = []
-    cleanTweet = []
-    sentimentTweet = []
-    for i in range(0, len(tweets)):
-        textTweet.append(tweets[i]['text'])
-    for i in range(0, len(tweets)):
-        cleanTweet.append(tweets[i]['clean tweet'])
-    for i in range(0, len(tweets)):
-        sentimentTweet.append(tweets[i]['sentiment'])
-
-    # Create a dataframe of collected tweets
-    df = pd.concat([
-        pd.DataFrame(textTweet, columns=['tweet']),
-        pd.DataFrame(cleanTweet, columns=['cleantweet'])
-    ],
-                   axis=1)
-    df = pd.concat(
-        [df, pd.DataFrame(sentimentTweet, columns=['sentiment'])], axis=1)
-
-    wcloud = ' '.join([i for i in df['cleantweet']])
-
-    wordcloud = WordCloud(width=1000,
-                          height=700,
-                          random_state=21,
-                          max_font_size=120).generate(wcloud)
-    # if os.path.exists("static/images/img_*.png"):
-    dir_path = os.path.dirname(__file__)
-
-    for fname in os.listdir(dir_path + '/static/css/images'):
-      if fname.startswith('img_'):
-        os.remove(dir_path + "/static/css/images/" + fname)
-
-    image_name = "img_{}.png".format(str(uuid.uuid4()))
-    wordcloud.to_file('static/css/images/{}'.format(image_name))
-
-    copyfile('templates/index.html', 'templates/copyindex.html')
-    f = open('templates/copyindex.html', 'a', encoding='utf-8')
-
-    f.write( "<img class='cloudimg' src='{{url_for('static', filename='css/images/%s') }}' alt='wordcloud' >" % (image_name))
-    f.write("</div>")
-    f.write("""<div class="pos" id="posid">
-       <h1 class="posHeading">Recent Positive Tweets</h1>
-      <div class="subdiv">
-                 """)
-    for tweet in ptweets[:10]:
-        f.write("<p>" + tweet['text'] + "</p>\n")
-    f.write("</div>\n")
-    f.write("</div>\n")
-
-    f.write("""<div class="neg" id="negid">
-       <h1 class="negHeading">Recent Negative Tweets</h1>
-      <div class="subdiv">
-    """)
-    for tweet in ntweets[:10]:
-        f.write("<p>" + tweet['text'] + "</p>\n")
-    f.write("</div>\n")
-    f.write("</div>\n")
-
-    f.write("""<div class="neu" id="neuid">
-      <h1 class="neuHeading">Recent Neutral Tweets</h1>
-      <div class="subdiv">
-            """)
-    for tweet in neutweets[:10]:
-        f.write("<p>" + tweet['text'] + "</p>\n")
-    f.write("</div>\n")
-    f.write("</div>\n")
-
-    f.write("""
-       <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-    
-          <script type='text/javascript'>
-          google.charts.load("current", { packages: ["corechart"] });
-          google.charts.setOnLoadCallback(drawPie);
-
-          function drawPie() {
-            var data = new google.visualization.DataTable();
-            data.addColumn("string", "Sentiment");
-            data.addColumn("number", "Tweets");
-            data.addRows([
-              ["Positive", %s],
-              ["Negative", %s],
-            ]);
-
-            var options = {
-              title: "Sentiment Analysis by Percentage",
-              is3D: true,
-            pieSliceTextStyle: {color: "white" ,fontSize:20,bold: true},
-              colors: ["#51CDA0", "#ff5252", "#62B1FF"],
-              titleTextStyle: { color: "#000" ,fontSize:25,fontWeight:0},
-              backgroundColor: "#d0eaf7",
-              width: 700,
-              height: 500,
-            };
-
-            var chart1 = new google.visualization.PieChart(
-              document.getElementsByClassName("pieChart")[0]
-            );
-            chart1.draw(data, options);
-          }
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-          animationEnabled: true,
-          title: {
-            text: "Analysis by Count",
-            fontFamily: "monospace",
-            fontSize: 25,
-            fontWeight:900,
-          },
-          axisX: {
-            interval: 1,
-          },
-          axisY: {
-            title: "Counts ",
-            includeZero: true,
-          },
-          data: [
-            {
-              type: "bar",
-              dataPoints: [
-                { label: "Positive", y: %s,},
-                {label: "Negative",y: %s, },
-                { label: "Neutral", y: %s, },
-              ],
-            },
-          ],
-        });
-        chart.render();
-
-       </script>""" % (len(ptweets), len(ntweets),len(ptweets), len(ntweets),len(neutweets)))
-
-    f.write(""" <!-- Footer --><i class="place icon fas fa-long-arrow-alt-down fa-3x"></i>
-      <footer class="colored-section" id="footer">
-      <!-- <p class="textplace">Contact</p> -->
-      <div class="container-fluid">
-        <p>
-          © Copyright 2021 
-          <span class="team">
-            from_pandeys_import_
-            <a class="mailbtn" href="mailto:rajneeshpandey1708@gmail.com">R</a>
-            <a class="mailbtn" href="mailto:satyarth2002pandey@gmail.com">S</a>
-          </span>
-        </p>
-        </div>
-        </footer>
-    </body>
-    </html>
-    """)
-    f.close()
